ea9d7542c26ebb1cdfe5f84d535556215e3b9ef6
codingkohli <aditya.12.kohli@gmail.com>
2018-02-18 11:40:07 -0500

    parsing of Create Table

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -1,21 +1,65 @@
 package edu.buffalo.www.cse4562;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.parser.ParseException;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
+import com.sun.org.apache.bcel.internal.generic.Select;
+
 
 
 public class Main {
+	// Declaring the sample data and datapath query
 	public static final String dataPath = "./R.csv";
+	public static final String dataQuery = "CREATE TABLE Red (A int, B int, C int);";
+
 	public static void main(String[] main) throws IOException {
 		System.out.println("Hello, World");
-		System.out.println(System.getProperty("user.dir"));
-		dataConfig(dataPath);
+		//System.out.println(System.getProperty("user.dir"));
+		//dataConfig(dataPath);
+
+		// Flow for the query statements
+		Reader in = new StringReader(dataQuery);
+		// Giving the input into a JSQLParser
+		CCJSqlParser parser = new CCJSqlParser(in);
+		try{
+			Statement statement = parser.Statement();
+			//System.out.println(statement);
+			if (statement instanceof Select)
+			{
+				System.out.println("Select Logic to handle string");
+			}
+			else if (statement instanceof CreateTable)
+			{
+				System.out.println("Create Table Logic");
+				CreateTable createStatement = (CreateTable)statement;
+				System.out.println(createStatement);
+				boolean result = dataObjects(createStatement);
+				System.out.println(result);
+			}
+			else
+			{
+				System.out.println("Not a valid statement");
+			}
+		}
+		catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	/* Function to play around with parsing the CSV
@@ -25,8 +69,7 @@ public class Main {
 	{
 		try(
 				Reader pathReader = Files.newBufferedReader(Paths.get(dataPath));
-				/* Reading a file when there is no header specified
-				 * */
+				/* Reading a file when there is no header specified */
 				CSVParser csvParser = new CSVParser(pathReader, CSVFormat.newFormat('|'));
 				)
 				{
@@ -40,4 +83,20 @@ public class Main {
 			}
 				}
 	}
+
+	/* Function to perform action on create table statement */
+	public static boolean dataObjects(CreateTable createStatement)
+	{
+		Table tableName = createStatement.getTable();
+		List<ColumnDefinition> columns =  createStatement.getColumnDefinitions();
+		System.out.println(tableName);
+		System.out.println(columns);
+		for (ColumnDefinition column : columns )
+		{
+			System.out.println(column.getColumnName() + " with data type : " + column.getColDataType());
+
+		}
+		return true;
+	}
+
 }
