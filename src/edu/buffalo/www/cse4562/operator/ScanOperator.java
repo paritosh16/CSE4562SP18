bd483a126845614de939e6a7fef64f3680eb48b6
codingkohli <aditya.12.kohli@gmail.com>
2018-02-19 18:02:44 -0500

    Basic constructor of ScanOperator

--- src/edu/buffalo/www/cse4562/operator/ScanOperator.java
+++ src/edu/buffalo/www/cse4562/operator/ScanOperator.java
@@ -5,6 +5,8 @@ package edu.buffalo.www.cse4562.operator;
 
 public class ScanOperator {
 	/* Overloading the constructor for the method*/
+
+
 	public ScanOperator()
 	{
 		/* Default constructor : just assigns the memory*/
@@ -17,4 +19,5 @@ public class ScanOperator {
 
 	}
 
+	/* getSchema and get Alias */
 }
