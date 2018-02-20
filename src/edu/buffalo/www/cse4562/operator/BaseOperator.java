d342df6d5a9108ad847baa209cd132f88fd0fbcd
codingkohli <aditya.12.kohli@gmail.com>
2018-02-20 01:41:36 -0500

    Overator Overloading to assign the memory

--- src/edu/buffalo/www/cse4562/operator/BaseOperator.java
+++ src/edu/buffalo/www/cse4562/operator/BaseOperator.java
@@ -1,5 +1,16 @@
 package edu.buffalo.www.cse4562.operator;
 
 public class BaseOperator {
+	Object [] rowRecord;
+
+	public BaseOperator()
+	{
+		// Does nothing : just assigns the memory
+	}
+	/* Constructor initializes the size of the rowRecord which would be typecasted*/
+	public BaseOperator(int rowSize)
+	{
+		rowRecord = new Object[rowSize];
+	}
 
 }
