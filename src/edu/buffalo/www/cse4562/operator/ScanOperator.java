879c760a6bc275146164a6fa38f93191b2a74998
codingkohli <aditya.12.kohli@gmail.com>
2018-02-20 11:46:28 -0500

    Adding the close and hasnext method

--- src/edu/buffalo/www/cse4562/operator/ScanOperator.java
+++ src/edu/buffalo/www/cse4562/operator/ScanOperator.java
@@ -1,22 +1,49 @@
 package edu.buffalo.www.cse4562.operator;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Iterator;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-
+import edu.buffalo.www.cse4562.Main;
 import edu.buffalo.www.cse4562.TableSchema;
 
 /* Imported Libraries specific to scan*/
 
 
-public class ScanOperator {
+public class ScanOperator implements Iterator<BaseOperator>{
 	/* Overloading the constructor for the method*/
 	TableSchema tabSchema;
+	BufferedReader br ;
+	Boolean oFlag = false;
+	String line = "";
+	BaseOperator record ;
+	public ScanOperator()
+	{
+		/* Default constructor : just assigns the memory*/
+	}
+
+	/* Overloaded Constructor
+	 * The constructor gets the schema and sets the value of the Buffered reader*
+	 * Opens the buffered reader and opens the connection*/
+	public ScanOperator(String dataPath) throws IOException
+	{
+		String path = "./" + dataPath + ".csv";
+		try(
+				BufferedReader brVal = new BufferedReader(new FileReader(path));
+				)
+				{
+			/* Assigning the value to the Iterator and  records*/
+			this.br = brVal;
+			if(br != null)
+			{
+				this.oFlag = true;
+			}
+			this.tabSchema = Main.dataObjects.get(dataPath);
+			this.record = new BaseOperator(tabSchema.getTabColumns().size());
+
+				}
+	}
 
 	public TableSchema getTabSchema() {
 		return tabSchema;
@@ -26,26 +53,46 @@ public class ScanOperator {
 		this.tabSchema = tabSchema;
 	}
 
-	public ScanOperator()
-	{
-		/* Default constructor : just assigns the memory*/
+
+	@Override
+	public boolean hasNext() {
+
+		try {
+			return (this.line = br.readLine()) != null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return false;
+		}
 	}
 
-	/* Overloaded Constructor*/
-	public ScanOperator(String dataPath) throws IOException
-	{
-		try(
-				Reader pathReader = Files.newBufferedReader(Paths.get(dataPath));
-				/* Reading a file when there is no header specified */
-				CSVParser csvParser = new CSVParser(pathReader, CSVFormat.newFormat('|'));
-				)
-				{
-			/* Assigning the value to the Iterator and  records*/
-			Iterable<CSVRecord> records = csvParser.getRecords();
+	@Override
+	/* This function gets the next line */
+	public BaseOperator next() {
+		if (this.line != null)
+		{
+			String[] tempRecord = line.split("|");
+			for(int i = 0; i < this.tabSchema.getTabColumns().size();i++)
+			{
+				//this.record[i] = new Integer(arg0)
 
-				}
 
+			}
+		}
+		return null;
 	}
+
+	public Boolean close() throws IOException
+	{
+		if (!oFlag)
+		{
+			return false;
+		}
+
+		this.oFlag = false;
+		this.br.close();
+		return true;
+	}
+
 
 	/* getSchema and get Alias */
 }
