d8814b04aa8bcab095960d27ff5ca7a01f4bdb71
codingkohli <aditya.12.kohli@gmail.com>
2018-02-20 01:15:07 -0500

    Basic Structure of scan operator

--- src/edu/buffalo/www/cse4562/operator/ScanOperator.java
+++ src/edu/buffalo/www/cse4562/operator/ScanOperator.java
@@ -1,11 +1,30 @@
 package edu.buffalo.www.cse4562.operator;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import edu.buffalo.www.cse4562.TableSchema;
+
 /* Imported Libraries specific to scan*/
 
 
 public class ScanOperator {
 	/* Overloading the constructor for the method*/
+	TableSchema tabSchema;
 
+	public TableSchema getTabSchema() {
+		return tabSchema;
+	}
+
+	public void setTabSchema(TableSchema tabSchema) {
+		this.tabSchema = tabSchema;
+	}
 
 	public ScanOperator()
 	{
@@ -13,9 +32,18 @@ public class ScanOperator {
 	}
 
 	/* Overloaded Constructor*/
-	public ScanOperator(String dataPath)
+	public ScanOperator(String dataPath) throws IOException
 	{
+		try(
+				Reader pathReader = Files.newBufferedReader(Paths.get(dataPath));
+				/* Reading a file when there is no header specified */
+				CSVParser csvParser = new CSVParser(pathReader, CSVFormat.newFormat('|'));
+				)
+				{
+			/* Assigning the value to the Iterator and  records*/
+			Iterable<CSVRecord> records = csvParser.getRecords();
 
+				}
 
 	}
 
