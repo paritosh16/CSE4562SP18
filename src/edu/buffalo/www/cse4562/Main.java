f7256205abe1c419ce4d353bdacd6215bfc560c3
codingkohli <aditya.12.kohli@gmail.com>
2018-02-20 00:09:20 -0500

    Modifying the main method according to the Autograder

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -1,13 +1,30 @@
 package edu.buffalo.www.cse4562;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.HashMap;
+
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
 
 public class Main {
 	/* Hash Map containing the schema */
 	public static HashMap<String,TableSchema> dataObjects = new HashMap<>();
+	static String prompt = "$> "; // expected prompt
+	public static void main(String[] main) throws Exception {
+		// ready to read stdin, print out prompt
+		System.out.println(prompt);
+		System.out.flush();
 
-	public static void main(String[] main) {
-		System.out.println("Hello, World");
-
+		Reader in = new InputStreamReader(System.in);
+		CCJSqlParser parser = new CCJSqlParser(in);
+		Statement s;
+		// project here
+		while((s = parser.Statement()) != null){
+			System.out.println("Query Result");
+			// 	read for next query
+			System.out.println(prompt);
+			System.out.flush();
+		}
 	}
 }
