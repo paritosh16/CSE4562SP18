8753bd50ffa8c10708498191bda97e863b75ed9b
Karan Luthra <karanluthra06@gmail.com>
2018-02-22 22:56:08 -0500

    refactor eval()

--- src/edu/buffalo/www/cse4562/evaluator/evalOperator.java
+++ src/edu/buffalo/www/cse4562/evaluator/evalOperator.java
@@ -23,23 +23,18 @@ public class evalOperator extends Eval {
 
 	@Override
 	public PrimitiveValue eval(Column col) {
-		int colIndex = 0;
-		// Get the column list that maps to indices in the row for the schema.
-		List<ColumnDefinition> test = (tableSchema.getTabColumns());
-		for(int i = 0; i < test.size(); i++) {
-			// Get the column name from schema.
-			String schemaName = test.get(i).getColumnName().toString();
-			// Get the column name from the expression column.
-			String argumentName = col.getColumnName().toString();
-			if(schemaName.toUpperCase().equals(argumentName.toUpperCase())) {
-				// Found the column in the schema.
+		int colIndex = -1;
+		String argColumnName = col.getColumnName().toString().toUpperCase();
+		List<ColumnDefinition> columnList = (tableSchema.getTabColumns());
+		String schemaColName;
+		for(int i = 0; i < columnList.size(); i++) {
+			schemaColName = columnList.get(i).getColumnName().toString().toUpperCase();
+			if(schemaColName.equals(argColumnName)) {
 				// Grab the index at which the column definition is stored in the table schema.
-				colIndex = i;
+				//colIndex = i;
+				return (PrimitiveValue)this.currentRow[i];
 			}
 		}
-		// Grab the value of the required column with the help of index and the
-		// currently read row. Return whilst type casting to the PrimitiveValue
-		// data type.
-		return (PrimitiveValue) currentRow[colIndex];
+		return (PrimitiveValue)this.currentRow[colIndex];
 	}
 }
