eff443c64771f06621229f5d5464a94bb8f08570
Karan Luthra <karanluthra06@gmail.com>
2018-03-12 18:30:12 -0400

    extend BaseOperator to support 2 child operators

--- src/edu/buffalo/www/cse4562/operator/BaseOperator.java
+++ src/edu/buffalo/www/cse4562/operator/BaseOperator.java
@@ -7,16 +7,22 @@ import edu.buffalo.www.cse4562.TableSchema;
 // TODO make BaseOperator an abstract class if possible
 public class BaseOperator implements Iterator<Object[]> {
 	protected BaseOperator childOperator;
+	protected BaseOperator secondChildOperator;
 	private TableSchema tableSchema;
 
 	/**
 	 * @param childOperator
 	 */
-	public BaseOperator(BaseOperator childOperator, TableSchema tableSchema) {
+	public BaseOperator(BaseOperator childOperator, BaseOperator secondChildOperator, TableSchema tableSchema) {
 		super();
 		this.childOperator = childOperator;
+		this.secondChildOperator = secondChildOperator;
 		this.tableSchema = tableSchema;
 		this.tableSchema.setTabAlias(this.tableSchema.getTableName());
+	}
+
+	public BaseOperator(BaseOperator childOperator, TableSchema tableSchema) {
+		this(childOperator, null, tableSchema);
 	}
 
 	public TableSchema getTableSchema() {
