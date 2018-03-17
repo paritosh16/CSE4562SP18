7f5069070deb9e375a57470c5df19d62907482e7
Paritosh Walvekar <paritosh.walvekar@gmail.com>
2018-03-16 20:51:21 -0400

    Error print function to output queries.

--- src/edu/buffalo/www/cse4562/Main.java
+++ src/edu/buffalo/www/cse4562/Main.java
@@ -23,6 +23,7 @@ public class Main {
 			// System.out.println("Query Result");
 			try {
 				boolean success = queryProcessor.processOne(s);
+				System.err.println(s);
 				if (success) {
 					BaseOperator resultIterator = queryProcessor.getRootOperator();
 					String result;
