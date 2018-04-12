c7020bdfd9bc796741f2234589a5c2ec0fd40674
Paritosh Walvekar <paritosh.walvekar@gmail.com>
2018-04-12 01:54:56 -0400

    Right operator set to scan and left operator set to Join.

--- src/edu/buffalo/www/cse4562/operator/join/BlockNestedLoopJoin.java
+++ src/edu/buffalo/www/cse4562/operator/join/BlockNestedLoopJoin.java
@@ -11,7 +11,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
  *
  */
 public class BlockNestedLoopJoin implements Iterator<Object[]>{
-	final int block_size = 15;
+	final int block_size = 1000;
 	private Iterator<Object[]> leftIterator;
 	private Iterator<Object[]> rightIterator;
 	int leftTupleLength;
